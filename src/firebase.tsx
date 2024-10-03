e509faff7ce2583a4344eed2dc8599a09236cde4
janewise <janewise2001@gmail.com>
2024-10-03 22:35:48 +0630

    first commit

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -3,14 +3,14 @@ import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database"; // Import the Realtime Database
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAei2Enk5qD6cBtosYB66qQwzmUHzrEW1w",
-  authDomain: "bitbrawl-official-airdrop.firebaseapp.com",
-  databaseURL: "https://bitbrawl-official-airdrop-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "bitbrawl-official-airdrop",
-  storageBucket: "bitbrawl-official-airdrop.appspot.com",
-  messagingSenderId: "179762649507",
-  appId: "1:179762649507:web:fe42f84b205d2c0bca2216",
-  measurementId: "G-SVKSDKEN7M"
+  apiKey: "AIzaSyChieGJYU9TGzBlUljWjVqYF2Erb3w2ncE",
+  authDomain: "test404track.firebaseapp.com",
+  databaseURL: "https://test404track-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "test404track",
+  storageBucket: "test404track.appspot.com",
+  messagingSenderId: "23062101949",
+  appId: "1:23062101949:web:b402111fc08f76cf90dcd4",
+  measurementId: "G-214Z3KHXPD"
 };
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
